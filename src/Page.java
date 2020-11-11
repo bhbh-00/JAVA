e951c7ddbab9fc78a6c36637939bd966d9325076
bhbh-00 <72903216+bhbh-00@users.noreply.github.com>
2020-11-11 11:07:34 +0900

    Delete Page.java

--- src/Page.java
+++
@@ -1,8 +0,0 @@
-
-public class Page {
-	private int totalCntOfItems;
-	
-	private int startPageNo = 1; // 시작 페이지 번호
-	private int ItemsCntPage = 3; // 페이지당 출력 게시물 갯수
-	private int PageCntPerBlock = 5; // 한 페이지 블럭 당 페이지 갯수
-}
